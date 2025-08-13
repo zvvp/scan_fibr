5879c90c0b538dc9699ff3115cc6e74fbf1007ad
zvvp <zvvpopov@gmail.com>
2025-08-13 12:18:59 +0300

    Написал get_fintervals().

--- src/my_lib.rs
+++ src/my_lib.rs
@@ -1,12 +1,6 @@
-use ndarray::Array1;
 use std::fmt::Debug;
-use std::fs;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-// use std::path::Path;
-use crate::leads::Leads;
-// use encoding::all::ASCII;
-// use encoding::{Encoding, DecoderTrap};
 
 
 pub struct Ecg {
@@ -47,14 +41,11 @@ impl Ecg {
                         self.r_pos.push(split_line[0].parse::<u32>().unwrap());
                         self.intervals.push(split_line[1].parse::<i32>().unwrap());
                         let char_in_line = end_line[0].chars().nth(0);
-                        let char_end = match char_in_line {
-                            Some(val) => val,
-                            None => "A".chars().next().unwrap(),
-                        };
+                        let char_end = char_in_line.unwrap_or_else(|| "A".chars().next().unwrap());
                         if char_end == 'A' {
                             println!("{} {:?}", i, end_line);
                         }
-                        self.chars.push(char_end as char);
+                        self.chars.push(char_end);
                     }
                 }
             }
@@ -63,33 +54,77 @@ impl Ecg {
 
     pub fn get_fintervals(&mut self) {
         let mut max_diff = 0;
-        let mut mean_diff = 0;
         let mut mean_intervals = 0;
+        let trs = 0.99;
         let mut step: usize = 1;
-        let diff_intervals = get_diff_intervals(&self.intervals, 1);
+
         for i in 0..self.chars.len() {
-        // for (i, item) in self.intervals.iter().enumerate() {
             if step == 2 {
                 step = 1;
                 continue;
             }
             if (i > 3) && (i < self.intervals.len() - 3) {
-                let diff0 = diff_intervals[i];
-                let diff1 = diff_intervals[i + 1];
-                mean_diff = (diff0 + diff1) / 2;
+                let diff0 = &self.intervals[i] - &self.intervals[i - 1];
+                let diff1 = &self.intervals[i + 1] - &self.intervals[i];
+
                 if diff0 >= diff1 {
                     max_diff = diff0;
                 } else {
                     max_diff = diff1;
                 }
-                mean_intervals = (&self.intervals[i - 3] + &self.intervals[i - 2] + &self.intervals[i - 1] + &self.intervals[i + 2] + &self.intervals[i + 3]) / 5;
+                mean_intervals = (&self.intervals[i - 3]
+                    + &self.intervals[i - 2]
+                    + &self.intervals[i - 1]
+                    + &self.intervals[i + 2]
+                    + &self.intervals[i + 3])
+                    / 5;
                 if (self.chars[i] == 'V') && (self.chars[i + 1] == 'V') && (max_diff > 100) {
-                    self.fintervals.push(mean_intervals + ((&self.intervals[i] - mean_intervals) as f64 * 0.2) as i32);
+                    self.fintervals.push(
+                        mean_intervals
+                            + ((&self.intervals[i] - mean_intervals) as f64 * 0.2) as i32,
+                    );
                     step = 1;
                 } else if (self.chars[i] == 'V') && (self.chars[i + 1] != 'V') && (max_diff > 40) {
-                    self.fintervals.push(mean_intervals + ((&self.intervals[i] - mean_intervals) as f64 * 0.2) as i32);
-                    self.fintervals.push(mean_intervals + ((&self.intervals[i + 1] - mean_intervals) as f64 * 0.2) as i32);
+                    self.fintervals.push(
+                        mean_intervals
+                            + ((&self.intervals[i] - mean_intervals) as f64 * 0.2) as i32,
+                    );
+                    self.fintervals.push(
+                        mean_intervals
+                            + ((&self.intervals[i + 1] - mean_intervals) as f64 * 0.2) as i32,
+                    );
                     step = 2;
+                } else if self.chars[i] == 'N' {
+                    let tf: &Vec<i32> = &self.intervals[i - 1..i + 3].to_vec();
+                    let tf: Vec<f32> = tf.iter().map(|x| *x as f32).collect();
+                    let ref_t: Vec<f32> = vec![tf[0], tf[0] * 0.6, tf[0] * 1.3, tf[0],];
+                    let ref_t0: Vec<f32> = vec![tf[0], tf[0] * 0.6, tf[0], tf[0] * 0.6,];
+                    let ref_t1: Vec<f32> = vec![tf[0], tf[0] * 1.35, tf[0], tf[0] * 1.35,];
+
+                    let coef_cor = get_coef_cor(&ref_t, &tf);
+                    let coef_cor0 = get_coef_cor(&ref_t0, &tf);
+                    let coef_cor1 = get_coef_cor(&ref_t1, &tf);
+                    let vec_cor : Vec<f32> = vec![coef_cor, coef_cor0, coef_cor1];
+                    let max_cor = vec_cor.iter().fold(f32::MIN, |acc, &x| acc.max(x));
+                    let diff21 = tf[2] + tf[0] - 2.0 * tf[1];
+                    if (max_cor < trs) || (diff21 > 160.0) {
+                        self.fintervals.push(
+                            mean_intervals
+                                + ((&self.intervals[i] - mean_intervals) as f64 * 0.2) as i32,
+                        );
+                        self.fintervals.push(
+                            mean_intervals
+                                + ((&self.intervals[i + 1] - mean_intervals) as f64 * 0.2) as i32,
+                        );
+                        step = 2;
+                    } else {
+                        self.fintervals.push(
+                            mean_intervals
+                                + ((&self.intervals[i] - mean_intervals) as f64 * 0.2) as i32,
+                        );
+                        step = 1;
+                    }
+
                 } else {
                     self.fintervals.push(self.intervals[i]);
                     step = 1;
@@ -122,15 +157,14 @@ pub fn get_diff_intervals(intervals: &Vec<i32>, step_diff: usize) -> Vec<i32> {
     out
 }
 
-fn get_coef_cor(x: &Array1<f32>, y: &Array1<f32>) -> f32 {
+fn get_coef_cor(x: &Vec<f32>, y: &Vec<f32>) -> f32 {
     // Проверка, что длины массивов совпадают
     if x.len() != y.len() {
         panic!("Arrays must have the same length");
     }
-
     let n = x.len() as f32;
-    let mean_x = x.iter().sum::<f32>() / n;
-    let mean_y = y.iter().sum::<f32>() / n;
+    let mean_x = x.iter().map(|&xi| xi).sum::<f32>() / n;
+    let mean_y = y.iter().map(|&yi| yi).sum::<f32>() / n;
 
     // Сумма произведений отклонений
     let cov_xy = x
@@ -140,15 +174,14 @@ fn get_coef_cor(x: &Array1<f32>, y: &Array1<f32>) -> f32 {
         .sum::<f32>();
 
     // Дисперсии
-    let std_x = (x.iter().map(|&xi| (xi - mean_x).powi(2)).sum::<f32>() / (n - 1.0)).sqrt();
-
-    let std_y = (y.iter().map(|&yi| (yi - mean_y).powi(2)).sum::<f32>() / (n - 1.0)).sqrt();
+    let std_x = (x.iter().map(|&xi| (xi - mean_x).powi(2)).sum::<f32>() / (n - 1.0))
+        .sqrt();
+    let std_y = (y.iter().map(|&yi| (yi - mean_y).powi(2)).sum::<f32>() / (n - 1.0))
+        .sqrt();
 
     // Избегаем деления на ноль
     if std_x * std_y < std::f32::EPSILON {
         return 0.0;
     }
-
     cov_xy / (std_x * std_y)
 }
-
