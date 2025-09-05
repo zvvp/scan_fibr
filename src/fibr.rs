f25a27a3f6f2c628170237001c0352de7737b377
zvvp <zvvpopov@gmail.com>
2025-09-05 16:43:52 +0300

    Написал fibr.rs

---
+++ src/fibr.rs
@@ -0,0 +1,63 @@
+#[derive(Debug)]
+pub struct Fibr {
+    pub start_ind_arr: Vec<usize>,
+    pub stop_ind_arr: Vec<usize>,
+    pub diff_stop_start: Vec<usize>,
+}
+impl Fibr {
+    pub fn new(coef_fibr: &Vec<f32>, trs: &Vec<f32>, mask: &Vec<usize>) -> Fibr {
+        let mut fibr = Fibr {
+            start_ind_arr: vec![],
+            stop_ind_arr: vec![],
+            diff_stop_start: vec![],
+        };
+        fibr.get_start_stop(coef_fibr, trs, mask);
+        fibr
+    }
+
+    fn get_start_stop(&mut self, coef_fibr: &Vec<f32>, trs: &Vec<f32>, mask: &Vec<usize>) {
+        let mut start_ind: usize = 0;
+        let mut stop_ind: usize = 0;
+        let min_size: usize = 22;
+        let mut flag: bool = false;
+        if coef_fibr[0] > trs[0] {
+            self.start_ind_arr.push(start_ind);
+            flag = true;
+        }
+        for i in 0..coef_fibr.len() - 1 {
+            if (flag == false) && (coef_fibr[i] < trs[i]) && (coef_fibr[i + 1] > trs[i + 1]) && (mask[i] == 1) {
+                if i - stop_ind > min_size {
+                    start_ind = i;
+                    self.start_ind_arr.push(start_ind);
+                } else {
+                    if self.stop_ind_arr.len() > 0 {
+                        self.stop_ind_arr.pop().unwrap();
+                    }
+                }
+                flag = true;
+            } else if (flag == true) && (coef_fibr[i] > trs[i]) && (coef_fibr[i + 1] < trs[i + 1]) && (mask[i] == 1) {
+                if i - start_ind > min_size {
+                    stop_ind = i;
+                    self.stop_ind_arr.push(stop_ind);
+                } else {
+                    if self.start_ind_arr.len() > 0 {
+                        self.start_ind_arr.pop().unwrap();
+                    }
+                }
+                flag = false;
+            } else if (flag == true) && (i == coef_fibr.len() - 2) {
+                if i - start_ind > min_size {
+                    stop_ind = i + 1;
+                    self.stop_ind_arr.push(stop_ind);
+                } else {
+                    if self.start_ind_arr.len() > 0 {
+                        self.start_ind_arr.pop().unwrap();
+                    }
+                }
+            }
+        }
+        for i in 0..self.start_ind_arr.len() {
+            self.diff_stop_start.push(self.stop_ind_arr[i] - self.start_ind_arr[i]);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,31 @@ use ndarray_npy::write_npy;
 use crate::my_lib::{get_coef_fibr, get_coef_p};
 use time_param::TimeParam;
 use crate::disp::get_coef_disp;
+use crate::fibr::Fibr;
 
 mod leads;
 mod my_lib;
 mod time_param;
 mod zub_p;
 mod disp;
+mod fibr;
 
 fn main() {
     let time_param = TimeParam::new();
     let trs = &time_param.threshold;
-    // let clear_intervals = &time_param.clear_intervals;
     let coef_p = get_coef_p(&time_param);
     let coef_disp = get_coef_disp(&time_param);
     let coef_fibr = get_coef_fibr(&coef_p, &coef_disp, &time_param);
+    let mask: Vec<usize> = vec![1; coef_fibr.len()];
+    let fibr = Fibr::new(&coef_fibr, &trs, &mask);
+    println!("{:?}", fibr);
 
-    let trs: Vec<f64> = trs.iter().map(|&x| x as f64).collect();
-    let array: Array1<f64> = trs.into();
-    write_npy("trs.npy", &array).expect("Ошибка сохранения .npy файла");
-
-    let coef_fibr: Vec<f64> = coef_fibr.iter().map(|&x| x as f64).collect();
-    let array: Array1<f64> = coef_fibr.into();
-    write_npy("coef_fibr.npy", &array).expect("Ошибка сохранения .npy файла");
+    // let trs: Vec<f64> = trs.iter().map(|&x| x as f64).collect();
+    // let array: Array1<f64> = trs.into();
+    // write_npy("trs.npy", &array).expect("Ошибка сохранения .npy файла");
+    //
+    // let coef_fibr: Vec<f64> = coef_fibr.iter().map(|&x| x as f64).collect();
+    // let array: Array1<f64> = coef_fibr.into();
+    // write_npy("coef_fibr.npy", &array).expect("Ошибка сохранения .npy файла");
 
 }
